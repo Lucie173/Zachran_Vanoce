90f9fd40634b999d2fd7130096d91b30c8ee9b10
Lucie Krausova <krausovaxl@gmail.com>
2020-09-21 14:37:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let robot = {
+    element: document.getElementById('robot'),
+    x: 0,
+    y: 495,
+    sirka: 135,
+    vyska: 105,
+    rychlost: 10
+}
+
+let sane = {
+    element: document.getElementById('sane'),
+    x: 0,
+    y: 10,
+    sirka: 165,
+    vyska: 104,
+    rychlost: 3
+}
